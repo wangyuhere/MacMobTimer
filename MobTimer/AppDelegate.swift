7089bf9176476bbd81e7bfdbe9fd55172a380785
Yu Wang <wangyuhere@gmail.com>
2015-09-25 20:15:48 +0200

    Initial Commit

---
+++ MobTimer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MobTimer
+//
+//  Created by Yu Wang on 25/09/15.
+//  Copyright © 2015 Yu Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

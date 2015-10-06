912d2b998e32aed5811ae8e99c901ae904dd84f6
Yu Wang <wangyuhere@gmail.com>
2015-10-06 18:06:04 +0200

    Add more sounds

---
+++ MobTimer/AddPlayerWindow.swift
@@ -0,0 +1,19 @@
+//
+//  AddPlayerWindow.swift
+//  MobTimer
+//
+//  Created by Yu Wang on 06/10/15.
+//  Copyright © 2015 Yu Wang. All rights reserved.
+//
+
+import Cocoa
+
+class AddPlayerWindow: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
